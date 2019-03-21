11fabf92ef0bc94d799816e2723eb2af17b7d7ab
tom_glb <goodmoringglb@163.com>
2019-03-21 15:06:33 +0800

    试试·1

--- src/main/java/com/atguigu/springboot000001/aop/TestAop.java
+++ src/main/java/com/atguigu/springboot000001/aop/TestAop.java
@@ -16,6 +16,9 @@ public class TestAop {
         System.out.println("hello3");
         System.out.println("hello4");
         System.out.println("hello5");
+        System.out.println("hello5");
+        System.out.println("hello5");
+        System.out.println("hello5");
     }
 
 }
